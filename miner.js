bce5b7e0c701f2b34d290bfa46fc717ce3c080a0
Jorge Aceves <jorge_aceves_martin@outlook.com>
2017-12-09 12:28:31 -0600

    Working miner

---
+++ miner.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const crypto = require('crypto');
+
+function doubleHash(message){
+	sha256 = hasher(message).digest();	
+	return hasher(sha256).digest('hex');
+}
+
+function hasher(message){
+	return crypto.createHash('sha256').update(message);
+}
+
+program
+	.version('0.1.0')
+	.option('-s, --start <number>', 'Start')
+	.option('-e, --end <number>', 'End')
+	.option('-t --target <number>', 'Target')
+	.option('-h, --header <header>', 'Header')
+	.parse(process.argv);
+
+
+let start = parseInt(program.start);
+const end = parseInt(program.end);
+const header = program.header;
+const message = [header];
+const targetVal = parseInt(program.target, 16);
+let messageVal = targetVal;
+let fullMessage = '';
+
+while(start < end){
+	message.push(start);
+	fullMessage = message.join('');
+	messageVal = parseInt(doubleHash(fullMessage), 16);
+	if(messageVal < targetVal){
+		break;
+	}
+	message.pop();
+	start++;
+}
+
+if(start == end){
+	process.exit(1);
+}
+
+console.log(start);
