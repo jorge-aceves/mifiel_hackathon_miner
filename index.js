ad08714432611ad557dcf6d6d3ce64c896d2c046
Jorge Aceves <jorge_aceves_martin@outlook.com>
2017-12-09 13:14:51 -0600

    Partial block header ready

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const WebSocket = require('ws');
-const sha256 = require('sha256');
 const axios = require('axios');
+const crypto = require('crypto')
+const exec = require('child_process')
 
 const params = {
   channel: 'MiniBlockchainChannel',
   nickname: 'saltygeorge',
-  uuid: 'a unique identifier (e.g 4547876e-c158-be87-9aa1-e6a9b5d9c063)',
+  uuid: 'bc70293f-81ed-4ced-8f45-8f807bbc301f',
   game: 'testnet',
-  version: 1,
-  message: 'Puto el que lo lea 3====D'
+  version: '1',
+  message: 'Puto el que lo lea 8====D'
 };
 
 const API = `https://gameathon.mifiel.com/api/v1/games/${params.game}`;
@@ -17,15 +18,82 @@ const ws = new WebSocket('wss://gameathon.mifiel.com/cable');
 
 let blocks, pool, target;
 
-getMerkleHash = () => {
+let miners = [];
+
+getMerkleHash = (transactions) => {
+  if (transactions.length == 1){
+    return transactions[0].hash;
+  }
+  arr = transactions;
   return '123-456-abc';
 }
 
 getPartialBlockHeader = () => {
   const lastBlock = blocks.pop();
-  const lastTransaction = pool.pop();
-  return `${params.version}|${lastBlock.hash}|${getMerkleHash()}|${target}|${params.message}|`;
+  let coinbaseTrans = {
+    inputs: [
+      {
+        prev_hash: "0000000000000000000000000000000000000000000000000000000000000000",
+        vout: -1,
+        script_sig: "12345678901234567890"
+      }
+    ],
+    outputs: [
+      {
+        value: 5000000000,
+        script: '03627eac6729a1f3f210dbfba4f9e21d6bfdce764e00b7559cc68a7551ddd839bf'
+      }
+    ]
+  }
+  const coinbaseHash = getTransactionHash(coinbaseTrans);
+  coinbaseTrans.hash = coinbaseHash;
+  const transactions = [coinbaseTrans];
+  return `${params.version}|${lastBlock.hash}|${getMerkleHash(transactions)}|${target}|${params.message}|`;
 }
+
+reverseString = (str) => {
+  let l=str.length, s='';
+  while(l > 0) {
+    l--;
+    s+= str[l];
+  }
+  return s;
+}
+
+getTransactionHash = (transaction) => {
+ let inputt = Buffer.concat(transaction.inputs.map(input => {
+   return Buffer.concat([
+     new Buffer(input.prev_hash),
+     new Buffer(input.script_sig),
+     new Buffer(`${input.vout}`)
+   ]);
+ }));
+ let outputt = Buffer.concat(transaction.outputs.map(output => {
+   return Buffer.concat([
+     new Buffer(`${output.value}`),
+     new Buffer(`${output.script.length}`),
+     new Buffer(output.script)
+   ]);
+ }));
+ const buffers = Buffer.concat([
+    new Buffer(params.version),
+    new Buffer(`${transaction.inputs.length}`),
+    inputt,
+    new Buffer(`${transaction.outputs.length}`),
+    outputt,
+    new Buffer('0')
+  ]);
+ return reverseString(crypto.createHash('sha256').update(buffers).digest('hex'));
+}
+
+callMiners = (blockHeader) => {
+
+}
+
+stopMiners = () => {
+
+}
+
 
 ws.on('open', () => {
   const data = {
@@ -47,8 +115,8 @@ ws.on('open', () => {
           .get(`${API}/target`)
           .then((responseTarget) => {
             target = responseTarget.data.target;
-
-            getPartialBlockHeader();
+            console.log("nos llego un bloque")
+            console.log(getPartialBlockHeader());
           })
       });
     });
