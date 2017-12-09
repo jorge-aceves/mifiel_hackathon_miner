ac36bf7e376631c13c0b8d9cb112c2b81ea2ebd3
ezavile <ezavile@gmail.com>
2017-12-09 12:07:35 -0600

    add partialBlockHeader method

--- index.js
+++ index.js
@@ -6,27 +6,57 @@ const params = {
   channel: 'MiniBlockchainChannel',
   nickname: 'saltygeorge',
   uuid: 'a unique identifier (e.g 4547876e-c158-be87-9aa1-e6a9b5d9c063)',
-  game: 'testnet'
+  game: 'testnet',
+  version: 1,
+  message: 'Puto el que lo lea 3====D'
 };
 
 const API = `https://gameathon.mifiel.com/api/v1/games/${params.game}`;
 
 const ws = new WebSocket('wss://gameathon.mifiel.com/cable');
 
-let lastBlock;
+let blocks, pool, target;
+
+getMerkleHash = () => {
+  return '123-456-abc';
+}
+
+getPartialBlockHeader = () => {
+  const lastBlock = blocks.pop();
+  const lastTransaction = pool.pop();
+  return `${params.version}|${lastBlock.hash}|${getMerkleHash()}|${target}|${params.message}|`;
+}
 
 ws.on('open', () => {
   const data = {
     command: 'subscribe',
     identifier: JSON.stringify(params)
   }
+
   axios
     .get(`${API}/blocks`)
-    .then((response) => {
-      lastBlock = response;
-    })
+    .then((responseBlocks) => {
+      blocks = responseBlocks.data;
+
+      axios
+      .get(`${API}/pool`)
+      .then((responsePool) => {
+        pool = responsePool.data;
+
+        axios
+          .get(`${API}/target`)
+          .then((responseTarget) => {
+            target = responseTarget.data.target;
+
+            getPartialBlockHeader();
+          })
+      });
+    });
+
   ws.send(JSON.stringify(data));
 });
+
 ws.on('message', (event) => {
-  console.log(event)
+  // console.log(event)
 });
+
